d0d022b0633491988beeb88f0449dbdb66cd3c69
Atharva Phatak <athp456@gmail.com>
2021-07-02 15:59:37 +0530

    mock torchvision for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information -----------------------------------------------------
 
 project = "TorchFlare"
-copyright = "2021, Atharva Phatak"
+project_copyright = "2021, Atharva Phatak"
 author = "Atharva Phatak"
 
 # The full version, including alpha/beta/rc tags
